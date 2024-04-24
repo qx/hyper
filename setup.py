a4d8f5fb21236c86e72f872ab6c4fa09760fdda4
kk <admin@example.com>
2024-04-24 08:40:01 +0800

    No commit message provided

--- setup.py
+++ setup.py
@@ -76,9 +76,12 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
-    install_requires=[
-        'h2>=2.4,<3.0,!=2.5.0', 'hyperframe>=3.2,<4.0', 'rfc3986>=1.1.0,<2.0', 'brotlipy>=0.7.0,<1.0'
-    ],
+   install_requires=[
+    'h2>=2.4,<3.0,!=2.5.0',
+    'hyperframe @ git+https://github.com/qx/hyperframe.git#egg=hyperframe',
+    'rfc3986>=1.1.0,<2.0',
+    'brotlipy>=0.7.0,<1.0'
+],
     tests_require=['pytest', 'requests', 'mock'],
     cmdclass={'test': PyTest},
     entry_points={
